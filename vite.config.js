90fdf5e3d4ea0c5516914c9b393ff768e94de072
Yashika Patil <yashika.patil@pristine-code.com>
2025-09-04 10:28:10 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    port: 5182
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  server: {
+    open: true,
+    port: 5182,
+    historyApiFallback: true,
+  },
+
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      }
+    }
   }
 })
